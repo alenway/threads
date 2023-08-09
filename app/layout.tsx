2e9326724a6579f538c3c9637f6dbb9ec8939724
narendra <51484021+alenway@users.noreply.github.com>
2023-08-09 22:03:34 +0530

    change have done for demo

---
+++ app/layout.tsx
@@ -0,0 +1,11 @@
+import '.
+  children: React.ReactNode
+}) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </ClerkProvider>
+  )
+}
